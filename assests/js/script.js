b74c04f3c5e7de3cb3679002b8827cc89105e503
jcurran1289 <j.curran1289@gmail.com>
2021-09-11 18:29:48 +0000

    initial commit

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,1 +1,43 @@
-console.log("heellp")
+var continent = [ {
+"continent": "Asia",
+"total_cases": "20715441",
+"total_deaths": "337656",
+"total_cases_per_million": "4426.697",
+"total_deaths_per_million": "72.154"
+} ,
+{
+"continent": "Europe",
+"total_cases": "23807046",
+"total_deaths": "545714",
+"total_cases_per_million": "31838.354",
+"total_deaths_per_million": "729.811"
+} ,
+{
+"continent": "Africa",
+"total_cases": "2760450",
+"total_deaths": "65468",
+"total_cases_per_million": "2009.812",
+"total_deaths_per_million": "47.666"
+} ,
+{
+"continent": "North America",
+"total_cases": "23074544",
+"total_deaths": "512870",
+"total_cases_per_million": "38677.955",
+"total_deaths_per_million": "859.682"
+} ,
+{
+"continent": "South America",
+"total_cases": "13194159",
+"total_deaths": "418041",
+"total_cases_per_million": "30383.077",
+"total_deaths_per_million": "962.651"
+} ,
+{
+"continent": "Oceania",
+"total_cases": "31440",
+"total_deaths": "945",
+"total_cases_per_million": "727.442",
+"total_deaths_per_million": "21.865"
+}
+]
