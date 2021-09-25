457fa0ce7960f792352d1b085f8b3123f1661f75
jcurran1289 <j.curran1289@gmail.com>
2021-09-25 23:54:22 +0000

    testing maps api

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,8 +1,14 @@
 let continent=[]; //create empty array
+// let country=[]; //create empty array
+// let totalCases=[]; //create empty array
+// let highestCases=[];
+// let highestCasesCountry=[];
 
 window.addEventListener("load", function(){
     getData() ;
-    timeLineChart();
+    myMapfff(mapsLat,mapsLon);
+    
+    
 });
 
 
@@ -11,48 +17,82 @@ async function getData() {
     const response = await fetch(api_url);
     const data = await response.json();
     const countries = data.Countries;
-    // for(i=0; i< 10; i++){
-    //     pp.push(countries[i]);
-    // }
+    
+
     continent=countries;
+    // var maxCases = 0;
+    // for (var i = 0; i < countries.length; i++) {
+    //     if (countries[i].TotalConfirmed > maxCases) {
+    //         highestCasesCountry.push(countries[i].Country); 
+    //         highestCases.push(countries[i].TotalConfirmed); 
+    //     }
+    // }
+
+    getData2(continent[0].Country);
+     console.log(continent);
     onDone();
 }
 
-let countriesTimelineDate= [];
-let countriesTimelineCases= [];
-const api_url2 = 'https://api.covid19api.com/country/south-africa/status/confirmed?from=2020-03-01T00:00:00Z&to=2020-04-01T00:00:00Z';
+// let countriesTimelineDate= [];
+// let countriesTimelineCases= [];
+// const api_url2 = 'https://api.covid19api.com/country/south-africa/status/confirmed?from=2020-03-01T00:00:00Z&to=2020-04-01T00:00:00Z';
+// async function getData2(place) {
+//     // const response = await fetch(api_url2);
+//     const response = await fetch('https://api.covid19api.com/country/'+place+'/status/confirmed?from=2020-03-01T00:00:00Z&to=2020-04-01T00:00:00Z');
+//     const countriesTimeline = await response.json();
+//     // const countriesTimelineCountry = countriesTimeline.Country;
+//     for(i=0; i< countriesTimeline.length; i++){
+//         countriesTimelineDate.push(countriesTimeline[i].Date.slice(0, 10));
+//         countriesTimelineCases.push(countriesTimeline[i].Cases);
+//     }
+
+   
+// }
+
+
+
+// let mapsLat= [];
+// let mapsLon= [];
+// const api_url2 = 'https://api.covid19api.com/country/Afghanistan/status/confirmed?from=2020-03-01T00:00:00Z&to=2020-04-01T00:00:00Z';
+// async function getData2(place) {
+//     // const response = await fetch(api_url2);
+//     const response = await fetch('https://api.covid19api.com/country/'+place+'/status/confirmed?from=2020-03-01T00:00:00Z&to=2020-04-01T00:00:00Z');
+//     const mapsData = await response.json();
+//     // const countriesTimelineCountry = countriesTimeline.Country;
+    
+//         mapsLat.push(mapsData[0].Lon);
+//         mapsLon.push(mapsData[0].Lat);
+    
+//         console.log(place);
+  
+// }
+let mapsLat= [];
+let mapsLon= [];
+const api_url2 = 'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyDlDbfJtg5Gs7rrr59IwV0RgFxOUcEIS1s';
 async function getData2(place) {
     // const response = await fetch(api_url2);
     const response = await fetch('https://api.covid19api.com/country/'+place+'/status/confirmed?from=2020-03-01T00:00:00Z&to=2020-04-01T00:00:00Z');
-    const countriesTimeline = await response.json();
+    const mapsData = await response.json();
     // const countriesTimelineCountry = countriesTimeline.Country;
-    for(i=0; i< countriesTimeline.length; i++){
-        countriesTimelineDate.push(countriesTimeline[i].Date.slice(0, 10));
-        countriesTimelineCases.push(countriesTimeline[i].Cases);
-    }
-
-    // ee=countriesTimeline;
-    // continent=countries;
- 
-    onDone();
+    
+        mapsLat.push(mapsData[0].Lon);
+        mapsLon.push(mapsData[0].Lat);
+    
+        console.log(place);
+  
 }
-
 function onDone(){
-    //    alert("done")
+
      
         continentName.innerHTML=continent[0].Country;
         total_cases.innerHTML=continent[0].TotalConfirmed;
         total_deaths.innerHTML=continent[0].TotalDeaths;
         newCases.innerHTML=continent[0].NewConfirmed;
         newDeaths.innerHTML=continent[0].NewDeaths;
-        console.log(continent);
-        console.log(countriesTimelineDate);
-        console.log(countriesTimelineCases);
-    
+ 
+        
 }
-// getISS();
 
-// console.log(pp[0]);
 
 
 
@@ -124,16 +164,23 @@ document.getElementById("btn-right").addEventListener("click", function(){
         total_deaths.innerHTML=continent[0].TotalDeaths;
         newCases.innerHTML=continent[0].NewConfirmed;
         newDeaths.innerHTML=continent[0].NewDeaths;
+        // timeLineChart(continent[0].Country);
+        
     
         currentIndex=0;
        }else{
 
-   
+       
+        
         continentName.innerHTML=continent[nextIndex].Country;
         total_cases.innerHTML=continent[nextIndex].TotalConfirmed;
         total_deaths.innerHTML=continent[nextIndex].TotalDeaths;
         newCases.innerHTML=continent[nextIndex].NewConfirmed;
         newDeaths.innerHTML=continent[nextIndex].NewDeaths;
+        // getData2(continent[nextIndex].Country);
+        
+        console.log(mapsLat);
+        console.log(mapsLon);
     
         currentIndex = nextIndex;
         console.log(continent.length);
@@ -174,45 +221,66 @@ document.getElementById("btn-left").addEventListener("click", function(){
 
  function timeLineChart(){
  
-var ctx = document.getElementById('chart').getContext('2d');
+        var ctx = document.getElementById('chart').getContext('2d');
 
-getData2(ireland);
-var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-         labels: countriesTimelineDate,
-        // labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: 'New Cases',
-            fill: true,
-            // data: [12, 19, 3, 5, 2, 3],
-            data: countriesTimelineCases,
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
+        // getData2(place);
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: countriesTimelineDate,
+                // labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [{
+                    label: 'New Cases',
+                    fill: true,
+                    // data: [12, 19, 3, 5, 2, 3],
+                    data: countriesTimelineCases,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
             }
-        }
+        });
+};
+
+
+
+
+function myMapfff(Lat, Lon) {
+    var mapProp= {
+    center:new google.maps.LatLng(67.71,33.94),
+    //   center:new google.maps.LatLng(Lat,Lon),
+      zoom:5,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+ }
+
+
+ var country = "Germany";
+var geocoder;
+
+geocoder.geocode( {'address' : country}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+        map.setCenter(results[0].geometry.location);
     }
 });
-     
-};
