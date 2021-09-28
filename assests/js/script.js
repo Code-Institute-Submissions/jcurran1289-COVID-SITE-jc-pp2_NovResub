0938a0c639de83fb0b1e0c75f2f7aade392a4017
jcurran1289 <j.curran1289@gmail.com>
2021-09-28 23:39:13 +0000

    got chart working. now trying maps

--- assests/js/script.js
+++ assests/js/script.js
@@ -7,8 +7,9 @@ let continent=[]; //create empty array
 window.addEventListener("load", function(){
     getData() ;
     // myMapfff();
-    drawChart();
+    
     getTimeData();
+    
    
     // getData2(mapLocation) ;
     // timeLineChart();
@@ -36,28 +37,27 @@ async function getData() {
     // }
 
   
-     console.log(continent);
+     
     onDone();
 }
 
 let countriesTimelineDate= [];
-let countriesTimelineCases= [];
+// let countriesTimelineDate= [['Date', 'Cases']];
 const api_url2S = 'https://api.covid19api.com/country/south-africa/status/confirmed?from=2020-03-01T00:00:00Z&to=2020-04-01T00:00:00Z';
-async function getTimeData() {
+async function getTimeData(place) {
     // const response = await fetch(api_url2);
     // const response = await fetch('https://api.covid19api.com/country/'+place+'/status/confirmed?from=2020-03-01T00:00:00Z&to=2020-04-01T00:00:00Z');
-    const response = await fetch('https://api.covid19api.com/country/south-africa/status/confirmed?from=2020-03-01T00:00:00Z&to=2020-04-01T00:00:00Z');
+    const response = await fetch('https://api.covid19api.com/country/'+place+'/status/confirmed?from=2020-03-01T00:00:00Z&to=2020-04-01T00:00:00Z');
     const countriesTimeline = await response.json();
-    // const countriesTimelineCountry = countriesTimeline.Country;
+    countriesTimelineDate= [['Date', 'Cases']]
+    // countriesTimelineDate.push('Date', 'Cases');
     for(i=0; i< countriesTimeline.length; i++){
         // countriesTimelineDate.push(countriesTimeline[i].Date.slice(0, 10), countriesTimeline[i].Cases));
         // countriesTimelineCases.push(countriesTimeline[i].Cases);
         // console.log(countriesTimeline[i].Date.slice(0, 10), countriesTimeline[i].Cases))
         countriesTimelineDate.push([countriesTimeline[i].Date.slice(0, 10),countriesTimeline[i].Cases]);
-       
-   
     }
-    console.log(countriesTimeline[0].Date.slice(0, 10),',', countriesTimeline[0].Cases);
+    drawChart(countriesTimelineDate);
     console.log(countriesTimelineDate);
     
 }
@@ -90,12 +90,12 @@ async function getData2(place) {
     // const mapLocation= mapsData.results[0].geometry.location;
     // const countriesTimelineCountry = countriesTimeline.Country;
     
-    console.log(JSON.stringify(mapsData));
-    console.log(mapsData.results[0]);
+    // console.log(JSON.stringify(mapsData));
+    // console.log(mapsData.results[0]);
     // mapLocation.push(mapsData.results[0].geometry.location.lat);
-        // mapsLon.push(mapsData[0].Lat);
-        console.log(mapsData.results[0].geometry.location.lat);
-        console.log(mapsData.results[0].geometry.location.lng);
+        // // mapsLon.push(mapsData[0].Lat);
+        // console.log(mapsData.results[0].geometry.location.lat);
+        // console.log(mapsData.results[0].geometry.location.lng);
         // myMap(mapsData.results[0].geometry.location.lat, mapsData.results[0].geometry.location.lng);
         // myMap();
 
@@ -108,7 +108,7 @@ function onDone(){
         total_deaths.innerHTML=continent[0].TotalDeaths;
         newCases.innerHTML=continent[0].NewConfirmed;
         newDeaths.innerHTML=continent[0].NewDeaths;
-        console.log(continent[0].Country);
+        getTimeData(continent[0].Country)
         // getData2('ireland');
  
         
@@ -159,6 +159,7 @@ document.getElementById("search-button").addEventListener("click", function () {
             total_deaths.innerHTML=result.TotalDeaths;
             newCases.innerHTML=result.NewConfirmed;
             newDeaths.innerHTML=result.NewDeaths;
+            getTimeData(result.Country);
 
             currentIndex = newIndex; //set the new current index
         }else{
@@ -185,8 +186,8 @@ document.getElementById("btn-right").addEventListener("click", function(){
         total_deaths.innerHTML=continent[0].TotalDeaths;
         newCases.innerHTML=continent[0].NewConfirmed;
         newDeaths.innerHTML=continent[0].NewDeaths;
-        // timeLineChart(continent[0].Country);
-        
+        getTimeData(continent[0].Country);
+       
     
         currentIndex=0;
        }else{
@@ -198,7 +199,8 @@ document.getElementById("btn-right").addEventListener("click", function(){
         total_deaths.innerHTML=continent[nextIndex].TotalDeaths;
         newCases.innerHTML=continent[nextIndex].NewConfirmed;
         newDeaths.innerHTML=continent[nextIndex].NewDeaths;
-        // getData2(continent[nextIndex].Country);
+        getTimeData(continent[nextIndex].Country);
+       
       
         
         // console.log(mapsLat);
@@ -243,86 +245,24 @@ document.getElementById("btn-left").addEventListener("click", function(){
 
 });
 
- function timeLineChart(){
  
-        var ctx = document.getElementById('chart').getContext('2d');
-
-        // getData2(place);
-        var myChart = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: countriesTimelineDate,
-                // labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                datasets: [{
-                    label: 'New Cases',
-                    fill: true,
-                    // data: [12, 19, 3, 5, 2, 3],
-                    data: countriesTimelineCases,
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                }
-            }
-        });
-};
 
 
 
 
-function myMap() {
-    var mapProp= {
-      center:new google.maps.LatLng(53.1423672,-7.692053599999999),
-    // center:new google.maps.LatLng();
+let testd =[
+    ['Year', 'Sales'],
+    ['2004',  1000],
+    ['2005',  1170],
+    ['2006',  660],
+    ['2007',  1030]
+  ]
 
-    //  center:new google.maps.LatLng(lat,lng),
-    //   zoom:6
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
- }
- function drawChart() {
-    // var data = google.visualization.arrayToDataTable([
-    //   ['Year', 'Sales', 'Expenses'],
-    //   ['2004',  1000,      400],
-    //   ['2005',  1170,      460],
-    //   ['2006',  660,       1120],
-    //   ['2007',  1030,      540]
-    // ]);
-
- 
-    var data = new google.visualization.DataTable();
-data.addColumn('string', 'Task');
-data.addColumn('number', 'Hours per Day');
-data.addRows([
-    countriesTimelineDate
-]);
-    var options = {
-      title: 'Company Performance',
-      curveType: 'function',
-      legend: { position: 'bottom' }
-    };
-
-    var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
-
-    chart.draw(data, options);
-  }
+  let testd2 =[
+    ['Year', 'Sales'],
+    ['2004',  10],
+    ['2005',  11],
+    ['2006',  60],
+    ['2007',  10]
+  ]
+  console.log(testd);
